1ecd9b2cd263cb53a67157c7d4e93fb2575d1db7
kokanek <kapeel.kokane@gmail.com>
2023-03-25 23:25:47 +0530

    testing the asd9 bot

--- pages/api/asd9bot.js
+++ pages/api/asd9bot.js
@@ -3,6 +3,9 @@ const expectedAuthHeader = process.env.ASD9_BOTH_AUTH_HEADER;
 
 // asd9bot bot handler
 export default async function handler(req, res) {
+  const { json } = req;
+  console.log('json: ', json);
+  console.log('auth header: ', req.headers.authorization);
   if (req.headers.authorization != `Bearer ${expectedAuthHeader}`) {
     res.status(400).json({ message: 'Authentication error' });
   } else {
