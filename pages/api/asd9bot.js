8df74a0555d895b7aa2025de4622cf8896aed081
kokanek <kapeel.kokane@gmail.com>
2023-03-27 12:46:42 +0530

    modyfing asdbot to show body

--- pages/api/asd9bot.js
+++ pages/api/asd9bot.js
@@ -3,8 +3,8 @@ const expectedAuthHeader = process.env.ASD9_BOTH_AUTH_HEADER;
 
 // asd9bot bot handler
 export default async function handler(req, res) {
-  const { json } = req;
-  console.log('json: ', json);
+  const { body } = req;
+  console.log('json body: ', body);
   console.log('auth header: ', req.headers.authorization);
   if (req.headers.authorization != `Bearer ${expectedAuthHeader}`) {
     res.status(400).json({ message: 'Authentication error' });
